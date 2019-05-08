ee49282e188f03e295d0bb3aa3cfa0a82b83be5a
Jakub P. Lech <47220449+Jakub-L@users.noreply.github.com>
2019-05-08 17:01:19 +0100

    Add index of components

---
+++ components/index.js
@@ -0,0 +1,10 @@
+import AddPhotoScreen from './AddPhotoScreen';
+import AddPinScreen from './AddPinScreen';
+import BasicARScene from './BasicARScene';
+import HomeScreen from './HomeScreen';
+import PinScreen from './PinScreen';
+import StaticARScene from './StaticARScene';
+
+export {
+  AddPhotoScreen, AddPinScreen, BasicARScene, HomeScreen, PinScreen, StaticARScene,
+};
