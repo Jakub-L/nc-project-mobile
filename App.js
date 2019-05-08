2b3b9fbeb232fa77ec82820fd512ca9cf7cc2ce6
Jakub P. Lech <47220449+Jakub-L@users.noreply.github.com>
2019-05-08 22:10:52 +0100

    Add support for Arup font

--- App.js
+++ App.js
@@ -2,24 +2,31 @@ import React from 'react';
 import {
   Button, Image, Text, View,
 } from 'react-native';
-import { createStackNavigator, createAppContainer, Icon } from 'react-navigation';
-import welcomeScreenStyle from './styles/WelcomeScreen-style';
-import navigatorStyle from './styles/Navigator-style';
-import logo from './assets/logo-white-large.png';
+import { createStackNavigator, createAppContainer, } from 'react-navigation';
+import { Font } from 'expo';
 import {
   BasicARScene, AddPinScreen, AddPhotoScreen, HomeScreen,
 } from './components';
+import welcomeScreenStyle from './styles/WelcomeScreen-style';
+import navigatorStyle from './styles/Navigator-style';
 import arupStyles from './styles/arupStyles';
+import logo from './assets/logo-white-large.png';
 
 class WelcomeScreen extends React.Component {
+  state = { fontLoaded: false }
   static navigationOptions = { headerMode: 'none', headerStyle: { backgroundColor: arupStyles.blueBg } }
+
+  async componentDidMount() {
+    await Font.loadAsync({ 'times-new-roman': require('./assets/fonts/times-new-roman.ttf') })
+    this.setState({ fontLoaded: true });
+  }
 
   render() {
     const { navigation } = this.props;
     return (
       <View style={welcomeScreenStyle.container}>
         <Image source={logo} />
-        <Text>Welcome Screen</Text>
+        {this.state.fontLoaded ? <Text style={welcomeScreenStyle.appName}>Welcome Screen</Text> : null}
         <Button title="Go Home" onPress={() => navigation.navigate('Home')} />
       </View>
     );
