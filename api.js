19a0a0d0f32782d654b3a89725b5f86edd5f0d1a
Fraser Brebner <northcoders@northcoderss-MacBook-Pro.local>
2019-05-09 14:16:54 +0100

    will pass params of selected pin to pin screen when navigated

--- api.js
+++ api.js
@@ -4,6 +4,5 @@ const API_URL = 'https://site-seeing.herokuapp.com/api';
 
 export const getPins = async () => {
   const { data } = await axios.get(`${API_URL}/pins`);
-  console.log(data.pins)
   return data.pins;
 };
