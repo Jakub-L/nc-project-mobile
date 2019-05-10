a0523215734672350d9ee8ceebcf634f6ace5a04
Jakub P. Lech <47220449+Jakub-L@users.noreply.github.com>
2019-05-10 16:28:49 +0100

    Refactor Map.js

--- components/Map.js
+++ components/Map.js
@@ -1,40 +1,43 @@
 import React from 'react';
 import {
-  MapView, Location, Permissions, Marker,
+  MapView, Location,
 } from 'expo';
 
 export default class Map extends React.Component {
   state = {
-    mapRegion: {
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    },
-    locationResult: null,
-    location: { coords: { latitude: 37.78825, longitude: -122.4324 } },
-    markers: [],
+    location: { coords: { latitude: 57.794671, longitude: -1.547202 } },
+  };
+
+  componentDidMount() {
+    this.getLocationAsync();
+  }
+
+  getLocationAsync = async () => {
+    const location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
   };
 
   render() {
+    const { location } = this.state;
+    const { pins, navigation } = this.props;
     return (
       <MapView
         style={{ alignSelf: 'stretch', height: 650 }}
         region={{
-          latitude: this.state.location.coords.latitude,
-          longitude: this.state.location.coords.longitude,
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
         showsUserLocation
       >
-        {this.props.pins.map(pin => (
+        {pins.map(pin => (
           <MapView.Marker
             key={pin.pin_id}
             coordinate={{ latitude: Number(pin.latitude), longitude: Number(pin.longitude) }}
             title={pin.creator}
             description={pin.timestamp}
-            onPress={() => this.props.navigation.navigate('Pin', {
+            onPress={() => navigation.navigate('Pin', {
               pin,
             })
             }
@@ -43,21 +46,4 @@ export default class Map extends React.Component {
       </MapView>
     );
   }
-
-  componentDidMount() {
-    this._getLocationAsync();
-  }
-
-  _getLocationAsync = async () => {
-    const { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
-      this.setState({
-        locationResult: 'Permission to access location was denied',
-        location,
-      });
-    }
-
-    let location = await Location.getCurrentPositionAsync({});
-    this.setState({ locationResult: JSON.stringify(location), location });
-  };
 }
