a6293bc048cb911af01b791b3635094045b57e2d
Jakub P. Lech <47220449+Jakub-L@users.noreply.github.com>
2019-05-10 10:40:15 +0100

    Add user input for login functionality

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {
-  Button, Image, Text, View,
+  Image, Text, TextInput, TouchableOpacity, View,
 } from 'react-native';
-import { createStackNavigator, createAppContainer, } from 'react-navigation';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { Font } from 'expo';
 import {
   BasicARScene, AddPinScreen, AddPhotoScreen, HomeScreen,
@@ -13,21 +13,96 @@ import arupStyles from './styles/arupStyles';
 import logo from './assets/logo-white-large.png';
 
 class WelcomeScreen extends React.Component {
-  state = { fontLoaded: false }
-  static navigationOptions = { headerMode: 'none', headerStyle: { backgroundColor: arupStyles.blueBg } }
+  static navigationOptions = {
+    headerMode: 'none',
+    headerStyle: { backgroundColor: arupStyles.blueBg },
+  };
+
+  state = {
+    fontLoaded: false,
+    email: 'Ressie.Jacobs@gmail.com',
+    password: 'password',
+    emailDefault: 'Ressie.Jacobs@gmail.com',
+    passwordDefault: 'password',
+    loginFailed: false,
+  };
 
   async componentDidMount() {
-    await Font.loadAsync({ 'times-new-roman': require('./assets/fonts/times-new-roman.ttf') })
+    await Font.loadAsync({
+      'times-new-roman': require('./assets/fonts/times-new-roman.ttf'),
+    });
     this.setState({ fontLoaded: true });
+  }
+
+  dealWithDefaultsFocus = (field) => {
+    if (field === 'email') {
+      const { email, emailDefault } = this.state;
+      if (email === emailDefault) this.setState({ email: '' });
+    } else if (field === 'password') {
+      const { password, passwordDefault } = this.state;
+      if (password === passwordDefault) this.setState({ password: '' });
+    }
+  };
+
+  dealWithDefaultsBlur = (field) => {
+    if (field === 'email') {
+      const { email, emailDefault } = this.state;
+      if (email === '') this.setState({ email: emailDefault });
+    } else if (field === 'password') {
+      const { password, passwordDefault } = this.state;
+      if (password === '') this.setState({ password: passwordDefault });
+    }
+  };
+
+  attemptLogin = () => {
+    const { email, password } = this.state;
+    
+
   }
 
   render() {
     const { navigation } = this.props;
+    const { fontLoaded, email, password } = this.state;
     return (
       <View style={welcomeScreenStyle.container}>
         <Image source={logo} />
-        {this.state.fontLoaded ? <Text style={welcomeScreenStyle.appName}>SiteSeeing</Text> : null}
-        <Button title="Go Home" onPress={() => navigation.navigate('Home')} />
+        {fontLoaded ? <Text style={welcomeScreenStyle.appName}>SiteSeeing</Text> : null}
+        <TextInput
+          onChangeText={email => this.setState({ email })}
+          onFocus={() => {
+            this.dealWithDefaultsFocus('email');
+          }}
+          onBlur={() => {
+            this.dealWithDefaultsBlur('email');
+          }}
+          value={email}
+          placeholder="Email"
+          textContentType="emailAddress"
+          placeholderTextColor={arupStyles['black-60']}
+          style={welcomeScreenStyle.textInput}
+        />
+        <TextInput
+          onChangeText={password => this.setState({ password })}
+          onFocus={() => {
+            this.dealWithDefaultsFocus('password');
+          }}
+          onBlur={() => {
+            this.dealWithDefaultsBlur('password');
+          }}
+          value={password}
+          placeholder="Password"
+          textContentType="password"
+          secureTextEntry
+          placeholderTextColor={arupStyles['black-60']}
+          style={welcomeScreenStyle.textInput}
+        />
+        <TouchableOpacity
+          style={welcomeScreenStyle.button}
+          onPress={() => navigation.navigate('Home')}
+          activeOpacity={0.8}
+        >
+          <Text style={welcomeScreenStyle.buttonText}>Log in</Text>
+        </TouchableOpacity>
       </View>
     );
   }
