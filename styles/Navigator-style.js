b31f663dd67d9edd6f86ed1ac39fd34ea1204d7b
Jakub P. Lech <47220449+Jakub-L@users.noreply.github.com>
2019-05-10 10:38:25 +0100

    Add iOS styling

--- styles/Navigator-style.js
+++ styles/Navigator-style.js
@@ -1,9 +1,9 @@
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 import arup from './arupStyles';
 
 export default StyleSheet.create({
   header: {
-    marginTop: -45,
+    marginTop: Platform.OS === 'ios' ? 0 : -45,
     backgroundColor: arup.blueBg,
   },
 });
