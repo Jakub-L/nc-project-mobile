b691b3808d7026987cef20bbb57f8054c28be170
Jakub P. Lech <47220449+Jakub-L@users.noreply.github.com>
2019-05-13 15:10:33 +0100

    Add styling for add pin screen

---
+++ styles/AddPinScreen-style.js
@@ -0,0 +1,72 @@
+import { StyleSheet } from 'react-native';
+import arup from './arupStyles';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  photoButtonsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-around',
+  },
+  image: {
+    height: 250,
+    width: 250,
+    margin: 10,
+    marginTop: 60,
+  },
+  noteField: {
+    flex: 1,
+    backgroundColor: arup.white,
+    margin: 10,
+    padding: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    textAlign: 'left',
+    textAlignVertical: 'top',
+    fontSize: 20,
+    color: arup['black-100'],
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: arup['black-60'],
+  },
+  icon: {
+    fontSize: 80,
+    color: arup.white,
+    textAlign: 'center',
+  },
+  iconButton: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 15,
+    margin: 10,
+    backgroundColor: arup.blueBg,
+    borderRadius: 5,
+  },
+  iconButtonText: {
+    color: arup.white,
+    textAlign: 'center',
+    marginTop: 10,
+    fontSize: 25,
+  },
+  disabledButton: {
+    height: 40,
+    margin: 10,
+    marginRight: 100,
+    marginLeft: 100,
+    padding: 10,
+    alignSelf: 'stretch',
+    backgroundColor: arup['black-60'],
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  disabledButtonText: {
+    fontSize: 20,
+    color: '#ffffff',
+  },
+});
